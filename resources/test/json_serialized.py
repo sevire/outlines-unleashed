656fff76fa795c9f3a8ad6e3f792581858e50862
Thomas Gaylard Admin <tbg-beanstalk@genonline.co.uk>
2020-02-04 18:05:19 +0000

    #171054200 Implement versioning approach for descriptors to enable backwards compatibility

--- resources/test/json_serialized.py
+++ resources/test/json_serialized.py
@@ -155,3 +155,166 @@ serialized_json_01 = \
         ]
     }
 }"""
+
+serialized_json_02 = \
+"""{
+    "header": {
+        "descriptor_version_number": "0.1"
+    }
+    "descriptor": {
+        "key_field_1": {
+            "primary_key": "yes",
+            "type": "string",
+            "field_value_specifier": "text_value",
+            "ancestry_matching_criteria": [
+                {
+                    "child_number": null,
+                    "text": null,
+                    "note": null,
+                    "text_tag": null,
+                    "note_tag": null
+                },
+                {
+                    "child_number": null,
+                    "text": null,
+                    "note": null,
+                    "text_tag": null,
+                    "note_tag": null
+                }
+            ]
+        },
+        "key_field_2": {
+            "primary_key": "yes",
+            "type": "string",
+            "field_value_specifier": "text_value",
+            "ancestry_matching_criteria": [
+                {
+                    "child_number": null,
+                    "text": null,
+                    "note": null,
+                    "text_tag": null,
+                    "note_tag": null
+                },
+                {
+                    "child_number": null,
+                    "text": null,
+                    "note": null,
+                    "text_tag": null,
+                    "note_tag": null
+                },
+                {
+                    "child_number": null,
+                    "text": null,
+                    "note": null,
+                    "text_tag": null,
+                    "note_tag": null
+                }
+            ]
+        },
+        "data_field_1": {
+            "primary_key": "no",
+            "type": "string",
+            "field_value_specifier": "text_value",
+            "ancestry_matching_criteria": [
+                {
+                    "child_number": null,
+                    "text": null,
+                    "note": null,
+                    "text_tag": null,
+                    "note_tag": null
+                },
+                {
+                    "child_number": null,
+                    "text": null,
+                    "note": null,
+                    "text_tag": null,
+                    "note_tag": null
+                },
+                {
+                    "child_number": null,
+                    "text": null,
+                    "note": null,
+                    "text_tag": null,
+                    "note_tag": null
+                },
+                {
+                    "child_number": 1,
+                    "text": null,
+                    "note": null,
+                    "text_tag": null,
+                    "note_tag": null
+                }
+            ]
+        },
+        "data_field_2": {
+            "primary_key": "no",
+            "type": "string",
+            "field_value_specifier": "text_value",
+            "ancestry_matching_criteria": [
+                {
+                    "child_number": null,
+                    "text": null,
+                    "note": null,
+                    "text_tag": null,
+                    "note_tag": null
+                },
+                {
+                    "child_number": null,
+                    "text": null,
+                    "note": null,
+                    "text_tag": null,
+                    "note_tag": null
+                },
+                {
+                    "child_number": null,
+                    "text": null,
+                    "note": null,
+                    "text_tag": null,
+                    "note_tag": null
+                },
+                {
+                    "child_number": 2,
+                    "text": null,
+                    "note": null,
+                    "text_tag": null,
+                    "note_tag": null
+                }
+            ]
+        },
+        "data_field_3": {
+            "primary_key": "no",
+            "type": "string",
+            "field_value_specifier": "text_value",
+            "ancestry_matching_criteria": [
+                {
+                    "child_number": null,
+                    "text": null,
+                    "note": null,
+                    "text_tag": null,
+                    "note_tag": null
+                },
+                {
+                    "child_number": null,
+                    "text": null,
+                    "note": null,
+                    "text_tag": null,
+                    "note_tag": null
+                },
+                {
+                    "child_number": null,
+                    "text": null,
+                    "note": null,
+                    "text_tag": null,
+                    "note_tag": null
+                },
+                {
+                    "child_number": 3,
+                    "text": null,
+                    "note": null,
+                    "text_tag": null,
+                    "note_tag": null
+                }
+            ]
+        }
+    }
+}"""
